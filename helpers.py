62291162268c6077517fc0c7992a67355dfa6533
Jet <taijunjet@hotmail.com>
2020-11-12 22:06:17 +0000

    Added helper class, strangely less FPS

---
+++ helpers.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+from sys import version
+import time
+import os
+import sys
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms.functional as TF
+
+from PIL import Image
+import matplotlib.pyplot as plt
+import cv2
+
+
+class helpers:
+    def __init__(self, mark_number, version_number, weights_location):
+        # training checkpoint variables
+        self.running_loss = 0
+        self.lowest_running_loss = 0
+
+        # weight file variables
+        self.weights_location = weights_location
+        self.mark_number = mark_number
+        self.version_number = version_number
+        self.weights_file = os.path.join(self.weights_location, f'weights/Version{self.version_number}/weights{self.mark_number}.pth')
+
+        # home directory
+        self.directory = os.path.dirname(__file__)
+
+
+
+    # simple call to reset internal running loss
+    def reset_running_loss(self):
+        self.running_loss = 0.
+
+
+
+    # call inside training loop
+    # helps to display training progress and save any improvement
+    def training_checkpoint(self, loss, iterations, epoch):
+        self.running_loss += loss
+
+        if iterations % 100 == 0:
+            # at the moment, no way to evaluate the current state of training, so we just record the current running loss
+            self.lowest_running_loss = (self.running_loss.item() if (iterations == 100) else self.lowest_running_loss)
+            
+            print(f'Epoch {epoch}; Batch Number {iterations}; Running Loss {self.running_loss.item()}; Lowest Running Loss {self.lowest_running_loss}')
+
+            # save the network if the current running loss is lower than the one we have
+            if(self.running_loss.item() < self.lowest_running_loss) and iterations > 1:
+                # save the net
+                self.lowest_running_loss = self.running_loss.item()
+                self.mark_number += 1
+
+                # regenerate the weights_file path
+                weights_file = os.path.join(self.weights_location, f'weights/Version{self.version_number}/weights{self.mark_number}.pth')
+
+                # reset the running loss for the next n batches
+                self.running_loss = 0.
+
+                # print the weight file that we should save to
+                print(F"New lowest point, saving weights to: {weights_file}")
+                return self.weights_file
+
+            # reset the running loss for the next n batches
+            self.running_loss = 0.
+        
+        # return -1 if we are not returning the weights file
+        return -1
+
+
+
+    # retrieves the latest weight file based on mark and version number
+    # weight location is location where all weights of all versions are stored
+    # version number for new networks, mark number for training
+    def get_latest_weight_file(self):
+
+        # while the file exists, try to look for a file one version later
+        while os.path.isfile(self.weights_file):
+            self.mark_number += 1
+            self.weights_file = os.path.join(self.weights_location, f'weights/Version{self.version_number}/weights{self.mark_number}.pth')
+
+        # once the file version doesn't exist, decrement by one and use that file
+        self.mark_number -= 1
+        self.weights_file = os.path.join(self.weights_location, f'weights/Version{self.version_number}/weights{self.mark_number}.pth')
+
+        # if there's no files, ignore
+        if self.mark_number > 0:
+            print(F"Using weights file: {self.weights_file}")
+            return self.weights_file
+        else:
+            print(F"No weights file found, generating new one during training.")
+            return -1
+
+
+
+#################################################################################################
+#################################################################################################
+# STATIC FUNCTIONS
+#################################################################################################
+#################################################################################################
+
+    @staticmethod
+    def get_device():
+        # select device
+        device = 'cpu'
+        if(torch.cuda.is_available()):
+            device = torch.device('cuda:0')
+        print('USING DEVICE', device)
+
+        return device
+
+    # enables peeking of the dataset
+    @staticmethod
+    def peek_dataset(dataloader):
+        dataiter = iter(dataloader)
+        user_input = None
+        while user_input != 'Y':
+            data, label = dataiter.next()
+            
+            print(label[0].shape)
+            print(data[0].shape)
+            plt.imshow(data[0].squeeze())
+            plt.show()
+            plt.imshow(label[0].squeeze())
+            plt.show()
+
+            user_input = input('Key in "Y" to end display, enter to continue...')
+
+
+    # converts saliency map to pseudo segmentation
+    # expects input of dim 2
+    # fastener_area_threshold is minimum area of output object BEFORE scaling to input size
+    @staticmethod
+    def saliency_to_contour(input, original_image, fastener_area_threshold, input_output_ratio):
+        # find contours in the image
+        threshold = input.detach().cpu().squeeze().numpy().astype(np.uint8)
+        contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        # filter contours below certain area
+        filtered_contours = []
+
+        for contour in contours:
+            if cv2.contourArea(contour) > fastener_area_threshold:
+                contour *= input_output_ratio
+                filtered_contours.append(contour)
+
+        # draw contours
+        contour_image = original_image.squeeze().to('cpu').detach().numpy()
+        cv2.drawContours(contour_image, filtered_contours, -1, 1, 1)
+
+        # return drawn image
+        return contour_image, len(filtered_contours)
+
--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
+from pickle import MARK
+from sys import version
 import time
 import os
 import sys
+
+import cv2
 import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms.functional as TF
 
-from PIL import Image
-import matplotlib.pyplot as plt
-import cv2
+from helpers import helpers
 
 from ImageLoader import ImageLoader
 from FasteNet_Net import FasteNet
@@ -22,23 +26,20 @@ from FasteNet_Net_HyperLite import FasteNet_HyperLite
 DIRECTORY = os.path.dirname(__file__)
 DIRECTORY2 = DIRECTORY # 'C:\WEIGHTS'
 
-VERSION_NUMBER = 4
-MARK_NUMBER = 385
+VERSION_NUMBER = 2
+MARK_NUMBER = 50
 
 BATCH_SIZE = 30
 
 THRESHOLD = 0.8
 
-# select device
-device = 'cpu'
-if(torch.cuda.is_available()):
-    device = torch.device('cuda:0')
-print('USING DEVICE', device)
+# instantiate helper object
+helpers = helpers(mark_number=MARK_NUMBER, version_number=VERSION_NUMBER, weights_location=DIRECTORY2)
+device = helpers.get_device()
 
 # holder for images and groundtruths and lowest running loss
 images = []
 truths = []
-lowest_running_loss = 1000
 
 # read in the images
 for i in range(3):
@@ -53,44 +54,20 @@ for i in range(3):
     images.append(image)
     truths.append(truth)
 
-# feed data to the ImageLoader and start the datalaoder to generate batch sizes
+# feed data to the ImageLoader and start the dataloader to generate batches
 dataset = ImageLoader(images, truths, (images[0].shape[0], images[0].shape[1]), crop_size=51)
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False, drop_last=True)
 
-# set this to one to view the output of the dataset
-if 0:
-    dataiter = iter(dataloader)
-    user_input = None
-    while user_input != 'Y':
-        data, label = dataiter.next()
-        
-        print(label[0].shape)
-        print(data[0].shape)
-        plt.imshow(data[0].squeeze())
-        plt.show()
-        plt.imshow(label[0].squeeze())
-        plt.show()
-
-        user_input = input('Key in "Y" to end display, enter to continue...')
+# uncomment this to one to view the output of the dataset
+# helpers.peek_dataset(dataloader=dataloader)
 
 # set up net
-FasteNet = FasteNet_HyperLite().to(device)
+FasteNet = FasteNet().to(device)
 
-# get the latest pth file and use as weights
-WEIGHT_FILE = os.path.join(DIRECTORY2, f'weights/Version{VERSION_NUMBER}/weights{MARK_NUMBER}.pth')
-
-while os.path.isfile(WEIGHT_FILE):
-    MARK_NUMBER += 1
-    WEIGHT_FILE = os.path.join(DIRECTORY2, f'weights/Version{VERSION_NUMBER}/weights{MARK_NUMBER}.pth')
-
-MARK_NUMBER -= 1
-WEIGHT_FILE = os.path.join(DIRECTORY2, f'weights/Version{VERSION_NUMBER}/weights{MARK_NUMBER}.pth')
-
-if MARK_NUMBER > 0:
-    FasteNet.load_state_dict(torch.load(WEIGHT_FILE))
-    print(F"Using weights file: {WEIGHT_FILE}")
-else:
-    print(F"No weights file found, generating new one during training.")
+# get latest weight file
+weights_file = helpers.get_latest_weight_file()
+if weights_file != -1:
+    FasteNet.load_state_dict(torch.load(weights_file))
 
 # set up loss function and optimizer
 loss_function = nn.MSELoss()
@@ -99,7 +76,7 @@ optimizer = optim.SGD(FasteNet.parameters(), lr=0.001, momentum=0.9)
 #  start training
 for epoch in range(0):
 
-    running_loss = 0.0
+    helpers.reset_running_loss()
 
     for i, data in enumerate(dataloader):
         # pull data out from dataloader
@@ -119,36 +96,16 @@ for epoch in range(0):
             plt.imshow(output[0].squeeze().to('cpu').detach().numpy())
             plt.show()
 
-
-        loss = loss_function(output, labels)
-
         # backprop
+        loss = loss_function(output, labels)
         loss.backward()
         optimizer.step()
-        running_loss += loss
-            
 
-        if i % 100 == 0:
-            # at the moment, no way to evaluate the current state of training, so we just record the current running loss
-            lowest_running_loss = (running_loss.item() if (i == 100) else lowest_running_loss)
-            
-            print(f'Epoch {epoch}; Batch Number {i}; Running Loss {running_loss.item()}; Lowest Running Loss {lowest_running_loss}')
-            
-            # save the network if the current running loss is lower than the one we have
-            if(running_loss.item() < lowest_running_loss) and i > 1:
-                # print current status
-                
-                # save the net
-                lowest_running_loss = running_loss.item()
-                MARK_NUMBER += 1
-                WEIGHT_FILE = os.path.join(DIRECTORY2, f'weights/Version{VERSION_NUMBER}/weights{MARK_NUMBER}.pth')
-                torch.save(FasteNet.state_dict(), WEIGHT_FILE)
+        weights_file = helpers.training_checkpoint(loss=loss, iterations=i, epoch=epoch)
 
-                # print the weight file that we saved to
-                print(F"New lowest point, saving weights to: {WEIGHT_FILE}")
-
-            # reset the running loss for the next n batches
-            running_loss = 0.
+        if weights_file != -1:
+            torch.save(FasteNet.state_dict(), weights_file)
+        
 
 
 
@@ -166,44 +123,29 @@ start_time = time.time()
 # set to true for inference
 for _ in range(frames_to_render):
     input = images[2].unsqueeze(0).unsqueeze(0).to(device)
-    output = FasteNet.forward(input)
+    saliency_map = FasteNet.forward(input)
     torch.cuda.synchronize()
 
-    # find contours in the image
-    image = output * 255
-    image = image.squeeze()
-    image = image.cpu().detach().numpy()
-    image = image.astype(np.uint8)
-    contours, hiearchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    # filter contours below certain area
-    filtered_contours = []
-
-    for contour in contours:
-        if cv2.contourArea(contour) > 5:
-            filtered_contours.append(contour)
-
-    # draw contours
-    contour_image = np.zeros_like(image)
-    cv2.drawContours(contour_image, filtered_contours, -1, 255, 1)
-    
+    # draw contours on original image
+    contour_image, contour_number = helpers.saliency_to_contour(input=saliency_map, original_image=input, fastener_area_threshold=5, input_output_ratio=8)
 
     # set to true to display images
     if 0:
-        comparison = truths[2].unsqueeze(0).unsqueeze(0)
-        figure = plt.figure()
-        figure.add_subplot(4, 1, 1)
-        plt.title('Input Image')
-        plt.imshow(input.squeeze().to('cpu').detach().numpy())
-        figure.add_subplot(4, 1, 2)
-        plt.title('Ground Truth')
-        plt.imshow(comparison.squeeze().to('cpu').detach().numpy())
-        figure.add_subplot(4, 1, 3)
-        plt.title('Predictions')
-        plt.imshow(output.squeeze().to('cpu').detach().numpy())
-        figure.add_subplot(4, 1, 4)
-        plt.title('Contours')
+        # comparison = truths[2].unsqueeze(0).unsqueeze(0)
+        # figure = plt.figure()
+        # figure.add_subplot(4, 1, 1)
+        # plt.title('Input Image')
+        # plt.imshow(input.squeeze().to('cpu').detach().numpy())
+        # figure.add_subplot(4, 1, 2)
+        # plt.title('Ground Truth')
+        # plt.imshow(comparison.squeeze().to('cpu').detach().numpy())
+        # figure.add_subplot(4, 1, 3)
+        # plt.title('Saliency Map')
+        # plt.imshow(saliency_map.squeeze().to('cpu').detach().numpy())
+        # figure.add_subplot(4, 1, 4)
+        # plt.title('Predictions')
         plt.imshow(contour_image)
+        print(f'Number of Fasteners in Image: {contour_number}')
 
 
         plt.show()
